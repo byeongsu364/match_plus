b72f8f1b038156dfd2ef1ffb8437c501e234173f
byeongsu364 <kbsyjysbs@gmail.com>
2025-09-19 10:46:21 +0900

    이거다

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
+import React from "react"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Stats from "./components/Stats"
+import Service from "./components/Service"
+import MatchList from "./components/MatchList"
+import Review from "./components/Review"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className=''>
-      
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Stats />
+      <Service />
+      <MatchList />
+      <Review />
+      <Footer />
+    </>
   )
 }
 
